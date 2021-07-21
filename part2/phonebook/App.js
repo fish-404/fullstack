a43348580d1a7795554dac4026e7cc123d5029b7
fish <fish404hsif@gmail.com>
2021-07-21 15:17:35 +0800

    Ex 2.15 - 2.18

--- part2/phonebook/App.js
+++ part2/phonebook/App.js
@@ -1,45 +1,81 @@
-import React, { useState } from 'react'
-
+import React, { useState, useEffect } from "react";
+import phonebookService from "./services/phoneBook";
 
 const Input = (props) => {
-	return (
-		<div>
-			{props.text}: <input value={props.value} onChange={props.onChange}/>
-		</div>
-	)
-}
+  return (
+    <div>
+      {props.text}: <input value={props.value} onChange={props.onChange} />
+    </div>
+  );
+};
 
 const Filter = (props) => {
-	return (
-		<>
-			<Input text="filter shown with" value={props.newFilter} onChange={props.handleFilterChange} />
-		</>
-	)
-}
+  return (
+    <>
+      <Input
+        text="filter shown with"
+        value={props.newFilter}
+        onChange={props.handleFilterChange}
+      />
+    </>
+  );
+};
 
 const AddForm = (props) => {
-	return (
-		<>
+  return (
+    <>
       <form onSubmit={props.addInfo}>
-				<Input text="name" value={props.newName} onChange={props.handleNameChange}/>				
-        <Input text="phone number" value={props.newPhone} onChange={props.handlePhoneChange}/>
+        <Input
+          text="name"
+          value={props.newName}
+          onChange={props.handleNameChange}
+        />
+        <Input
+          text="phone number"
+          value={props.newPhone}
+          onChange={props.handlePhoneChange}
+        />
         <div>
           <button type="submit">add</button>
         </div>
       </form>
-		</>
-	)
-}
+    </>
+  );
+};
 
-const Persons = ({filterToShow}) => {
-	return (
-		<>
-			{filterToShow.map(person => 
-				<p key={person.id}>{person.name} {person.phone}</p>
-			)}	
-		</>
-	)
-}
+const Person = (props) => {
+  const handleDelete = () => {
+    if (window.confirm(`Delet ${props.person.name} ?`)) {
+      phonebookService.deleteInfo(props.person.id).then((response) => {
+        props.deletePerson(props.person.id);
+        alert(`Deleted successfully! `);
+      });
+    }
+  };
+
+  return (
+    <>
+      <p>
+        {props.person.name} {props.person.phone}
+        <button onClick={handleDelete}>delete</button>
+      </p>
+    </>
+  );
+};
+
+const Persons = (props) => {
+  return (
+    <>
+      {props.filterToShow.map((person) => (
+        <Person
+          key={person.id}
+          person={person}
+          deletePerson={props.deletePerson}
+        />
+      ))}
+    </>
+  );
+};
 
 const App = () => {
   const [persons, setPersons] = useState([]);
@@ -49,73 +85,97 @@ const App = () => {
 
   useEffect(() => {
     console.log("effect");
-    axios
-      .get("https://sogz9-3001.sse.codesandbox.io/persons")
-      .then((response) => response.data)
-      .then((result) => {
-        console.log("result", result);
-        setPersons(result);
-      });
+    phonebookService
+      .getAll()
+      .then((initialPhoneBook) => setPersons(initialPhoneBook));
   }, []);
 
+  const handleNameChange = (event) => {
+    setNewName(event.target.value.trim());
+  };
 
-	const handleNameChange = (event) => {
-		setNewName(event.target.value)
-	}
+  const handlePhoneChange = (event) => {
+    setNewPhone(event.target.value.trim());
+  };
 
-	const handlePhoneChange = (event) => {
-		setNewPhone(event.target.value)
-	}
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
 
-	const handleFilterChange = (event) => {
-		setFilter(event.target.value)
-	}
+  function handleFilter(newFilter) {
+    console.log(newFilter);
+    if (newFilter === "") {
+      return persons;
+    } else {
+      const regex = new RegExp(newFilter, "i"); // case insensitive
+      //const result =  persons.filter(item=>regex.test(item.name))
+      //console.log("result", result)
+      return persons.filter((item) => regex.test(item.name));
+    }
+  }
 
-	function handleFilter(newFilter) {
-		console.log(newFilter)	
-		if (newFilter === "") {
-			return persons
-		}
-		else {
-			const regex = new RegExp(newFilter, 'i') // case insensitive
-			//const result =  persons.filter(item=>regex.test(item.name))
-			//console.log("result", result)
-			return persons.filter(item=>regex.test(item.name)) 
-		}
-	} 
+  const filterToShow = handleFilter(newFilter);
 
-	const filterToShow = handleFilter(newFilter)
+  const addInfo = (event) => {
+    event.preventDefault(); // aovid page refresh
+    const exitMessage = `${newName} is already added to phonebook`;
+    const updateMessage = `${exitMessage}, repalce the old number with a new one?`;
+    const noUpdateMessage = `${exitMessage}, and the phone has no change`;
 
-	const addInfo = (event) => {
-		event.preventDefault(); // aovid page refresh
-		const alertMessage = `${newName} is already added to phonebook`
+    const exitItem = persons.find((item) => item.name === newName);
 
-		if (persons.filter(item => item.name === newName).length > 0) {
-			alert(alertMessage)
-		}
-		else {
-			const infoObject = {
-				id: persons.length + 1, 
-				name: newName, 
-				phone: newPhone
-			}	
-	
-			setPersons(persons.concat(infoObject))
-			setNewName('')
-			setNewPhone('')
-		}
-	}
+    if (exitItem !== undefined) {
+      if (exitItem.phone === newPhone) {
+        alert(noUpdateMessage);
+      } else {
+        if (window.confirm(updateMessage)) {
+          const newItem = { ...exitItem, phone: newPhone };
+          phonebookService.updateInfo(newItem.id, newItem).then((response) => {
+            //console.log("test", response);
+            setPersons(
+              persons.map((person) =>
+                person.id !== newItem.id ? person : response
+              )
+            );
+          });
+        }
+      }
+    } else {
+      const infoObject = {
+        id: persons.length + 1,
+        name: newName,
+        phone: newPhone
+      };
+
+      phonebookService.addInfo(infoObject).then((returnInfo) => {
+        setPersons(persons.concat(returnInfo));
+        setNewName("");
+        setNewPhone("");
+      });
+    }
+  };
+
+  const deletePerson = (deleteId) => {
+    console.log("id", deleteId);
+    setPersons(persons.filter((person) => person.id !== deleteId));
+  };
 
   return (
     <div>
-			<h1>Phone book</h1>
-			<Filter newFilter={newFilter} handleFilterChange={handleFilterChange} />
+      <h1>Phone book</h1>
+      <Filter newFilter={newFilter} handleFilterChange={handleFilterChange} />
       <h2>add a new</h2>
-			<AddForm addInfo={addInfo} newName={newName} handleNameChange={handleNameChange} newPhone={newPhone} handlePhoneChange={handlePhoneChange} />
+      <AddForm
+        addInfo={addInfo}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newPhone={newPhone}
+        handlePhoneChange={handlePhoneChange}
+      />
       <h2>Numbers</h2>
-			<Persons filterToShow={filterToShow} />	
+      <Persons filterToShow={filterToShow} deletePerson={deletePerson} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
