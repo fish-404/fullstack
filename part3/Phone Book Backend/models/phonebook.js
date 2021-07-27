2cf2280d607b405ce0e7066b21b6074216fcf0c4
fish <fish404hsif@gmail.com>
2021-07-27 17:05:21 +0800

    Ex 3.15 - 3.18

--- part3/Phone Book Backend/models/phonebook.js
+++ part3/Phone Book Backend/models/phonebook.js
@@ -1,33 +1,125 @@
-const mongoose = require("mongoose");
-const url = process.env.MONGODB_URI;
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const cors = require("cors");
+const PhoneBook = require("../models/phonebook");
 
-console.log(`Connected to ${url}`);
+app.use(cors());
+app.use(express.json());
+morgan.token("myToken", function (req, res) {
+  return JSON.stringify({
+    name: req.body.name,
+    number: req.body.number
+  });
+});
 
-mongoose
-  .connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true
+app.use(
+  morgan(function (tokens, req, res) {
+    const logs = [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms"
+    ];
+    if (req.method === "POST") {
+      logs.push(tokens.myToken(req, res));
+    }
+
+    return logs.join(" ");
   })
-  .then((result) => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((error) => {
-    console.error(`Error connecting to MongoDB: ${error.message}`);
+);
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message);
+  if (error.name === "CastError" && error.kind === "ObjectId") {
+    return res.statsu(400).send({
+      error: "malformatted id"
+    });
+  }
+  next(error);
+};
+
+app.use(errorHandler);
+
+app.get("/api/persons", (req, res) => {
+  PhoneBook.find({}).then((persons) => {
+    res.json(persons);
+  });
+});
+
+app.get("/api/persons/:id", (req, res, next) => {
+  PhoneBook.findById(req.params.id)
+    .then((person) => {
+      if (person) {
+        res.json(person);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  PhoneBook.findByIdAndRemove(req.params.id)
+    .then((reslut) => {
+      res.status(204).end();
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+app.post("/api/persons", (req, res) => {
+  const body = req.body;
+
+  if (!body.name) {
+    return res.status(403).json({
+      error: "Name Missing"
+    });
+  }
+
+  if (!body.number) {
+    return res.status(403).json({
+      error: "Phone Number Missing"
+    });
+  }
+
+  const person = new PhoneBook({
+    name: body.name ? body.name : body.number,
+    number: body.number
   });
 
-const phoneBookSchema = new mongoose.Schema({
-  name: String,
-  number: String
+  person.save().then((savedPerson) => {
+    res.json(savedPerson);
+  });
 });
 
-phoneBookSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  }
+app.put("/api/persons/:id", (req, res, next) => {
+  const body = req.body;
+
+  const person = {
+    name: body.name,
+    number: body.number
+  };
+
+  PhoneBook.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then((updatePerson) => {
+      res.json(updatePerson);
+    })
+    .catch((error) => next(error));
 });
 
-module.exports = mongoose.model("PhoneBook", phoneBookSchema);
+app.get("/info", (req, res) => {
+  PhoneBook.find({}).then((persons) => {
+    res.send(`Phonebook has info for ${persons.length} people <br/> ${Date()}`);
+  });
+});
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
