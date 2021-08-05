ffde80111430f7209e28bc0ede922ddb865aebc2
fish <fish404hsif@gmail.com>
2021-08-05 16:49:38 +0800

    Ex 4.1 -  4.2

---
+++ part4/Blog List Application - Back/src/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const config = require("../utils/config");
+const mongoose = require("mongoose");
+const blogsRouter = require("../controllers/notes");
+
+const mongoUrl = config.MONGODB_URI;
+mongoose.connect(mongoUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+});
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/blogs", blogsRouter);
+
+module.exports = app;
