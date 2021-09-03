ee56f0d98a1cb020a3e540b6d0d7594d14e98bc7
fish <fish404hsif@gmail.com>
2021-09-03 11:57:16 +0800

    Ex 4.12

--- part4/Blog List Application - Back/src/app.js
+++ part4/Blog List Application - Back/src/app.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const config = require("../utils/config");
 const mongoose = require("mongoose");
 const blogsRouter = require("../controllers/blogs");
+const middleware = require("../utils/middleware");
 
 const mongoUrl = config.MONGODB_URI;
 mongoose.connect(mongoUrl, {
@@ -17,5 +18,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/blogs", blogsRouter);
+app.use(middleware.errorHandler)
 
 module.exports = app;
