def32152a917e721d3fd0e803efd14b8c9638e0b
fish <fish404hsif@gmail.com>
2021-09-27 16:55:10 +0800

    Refactoring tests

--- part4/Blog List Application - Back/tests/blog_api.test.js
+++ part4/Blog List Application - Back/tests/blog_api.test.js
@@ -3,7 +3,9 @@ const app = require("../src/app.js");
 const mongoose = require("mongoose");
 const Blog = require("../models/blog");
 const tests = require("./testcase");
+const helper = require("../utils/list_helper");
 
+const randomMax = 1000;
 const initBlogs = tests.blogs;
 
 beforeEach(async () => {
@@ -16,18 +18,36 @@ beforeEach(async () => {
 
 const api = supertest(app);
 
-describe("Blog test", () => {
+describe("Init Blogs", () => {
   test("all blogs are returned", async () => {
     const res = await api.get("/api/blogs");
     expect(res.body).toHaveLength(initBlogs.length);
   });
-  
-  test("has rename id property name", async () => {
+});
+
+describe("Analyze Blogs", () => {
+  test("total Likes", async () => {
+    const res = await api.get("/api/blogs");
+    expect(helper.totalLikes(res.body)).toEqual(helper.totalLikes(initBlogs));
+  });
+
+  test("favoriteBlog", async () => {
+    const res = await api.get("/api/blogs");
+    expect(helper.favoriteBlog(res.body).title).toEqual(
+      helper.favoriteBlog(initBlogs).title
+    );
+  });
+});
+
+describe("Specific Blog", () => {
+  test("rename id property name", async () => {
     const res = await api.get("/api/blogs");
     expect(res.body[0].id).toBeDefined();
   });
-  
-  test("post a new blog", async () => {
+});
+
+describe("Add Blog", () => {
+  test("post a new blog with valid parameters", async () => {
     const newBlog = {
       title: "Learn X in Y",
       author: "Conmunity",
@@ -45,13 +65,13 @@ describe("Blog test", () => {
     expect(res.body).toHaveLength(initBlogs.length + 1);
     expect(res.body).toEqual(
       expect.arrayContaining([expect.objectContaining(newBlog)])
-    ); 
+    );
   });
-  
+
   test("like default", async () => {
     const newBlog = {
       title: "Learn X in Y",
-      author: "Conmunity",
+      author: "Community",
       url: "https://learnxinyminutes.com/"
     };
 
@@ -68,7 +88,7 @@ describe("Blog test", () => {
       0
     );
   });
-  
+
   test("bad request", async () => {
     const newBlog = {
       author: "Community"
@@ -76,7 +96,9 @@ describe("Blog test", () => {
 
     await api.post("/api/blogs").send(newBlog).expect(400);
   });
-  
+});
+
+describe("Delete Blog", () => {
   test("delete blog", async () => {
     const res = await api.get("/api/blogs");
     const tobeDelId = helper.getRandomBlogId(res.body);
@@ -84,24 +106,19 @@ describe("Blog test", () => {
     const again = await api.get("/api/blogs");
     expect(again.body).toHaveLength(initBlogs.length - 1);
   });
-  
-    test("delete blog", async () => {
-    const res = await api.get("/api/blogs");
-    const tobeDelId = helper.getRandomBlogId(res.body);
-    await api.delete(`/api/blogs/${tobeDelId}`).expect(204);
-    const again = await api.get("/api/blogs");
-    expect(again.body).toHaveLength(initBlogs.length - 1);
-  });
+});
 
+describe("Update Blog", () => {
   test("update blog likes", async () => {
     const res = await api.get("/api/blogs");
     const tobeUpdateId = helper.getRandomBlogId(res.body);
     const newLikes = helper.getRandomInt(randomMax);
+    console.log(newLikes);
     const result = await api.put(
       `/api/blogs/${tobeUpdateId}/likes/${newLikes}`
     );
     expect(result.body.likes).toEqual(newLikes);
-  })
+  });
 });
 
 afterAll(() => {
