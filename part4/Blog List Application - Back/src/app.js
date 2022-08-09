5eb95d9b7fb80926ab38b49c3ead15725ebef7a0
fish <fish404hsif@gmail.com>
2022-08-09 02:30:03 +0000

    Ex4.15: add user controller

--- part4/Blog List Application - Back/src/app.js
+++ part4/Blog List Application - Back/src/app.js
@@ -5,8 +5,10 @@ const config = require("../utils/config");
 const mongoose = require("mongoose");
 const blogsRouter = require("../controllers/blogs");
 const middleware = require("../utils/middleware");
+const userRouter = require("../contollers/users");
 
 const mongoUrl = config.MONGODB_URI;
+
 mongoose.connect(mongoUrl, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -18,6 +20,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
 app.use(middleware.errorHandler)
 
 module.exports = app;
